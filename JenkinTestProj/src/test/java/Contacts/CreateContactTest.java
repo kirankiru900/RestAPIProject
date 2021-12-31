ba0da37883af7deef4080dfb4385f5e4beccebf9
AJAY KUMAR S <AJAY KUMAR S@DESKTOP-53K956R>
2021-12-31 18:32:19 +0530

    my new testing

---
+++ JenkinTestProj/src/test/java/Contacts/CreateContactTest.java
@@ -0,0 +1,10 @@
+package Contacts;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+@Test
+public void insideCreateContactTest() {
+	System.out.println("inside create contact");
+}
+}
---
+++ JenkinTestProj/src/test/java/Organization/CreateOrgTest.java
@@ -0,0 +1,10 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+@Test
+public void insideCreateOrgTest() {
+	System.out.println("inside create organization");
+}
+}
